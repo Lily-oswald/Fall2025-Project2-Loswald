12b7d2303ed6e98e97828c3718f1138f7af2c2cb
Lily-oswald <lilyoswald2005@gmail.com>
2025-10-09 12:06:03 -0500

    Initial commit (subfolder repo)

---
+++ wwwroot/js/script.js
@@ -0,0 +1,34 @@
+function displayCurrentTime() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const timeString = hours + ':' + minutes;
+    
+    $('#time').html(timeString).css('visibility', 'visible');
+    
+    $('#time').dialog({
+        title: 'Current Time',
+        modal: true,
+        width: 350
+    });
+}
+
+$(document).ready(function() {
+    // Time button click
+    $('#timeBtn').click(displayCurrentTime);
+    
+    // Background images array
+    const backgrounds = [
+        'https://images.unsplash.com/photo-1516741539888-96b4c96adf0c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'https://images.unsplash.com/photo-1576707948881-b485713fbad7?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'https://images.unsplash.com/photo-1559825481-12a05cc00344?q=80&w=1365&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'https://images.unsplash.com/photo-1672572483727-a198b6529189?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    ];
+    let currentBgIndex = 0;
+    
+    // Change background on header click
+    $('#headerClick').click(function() {
+        currentBgIndex = (currentBgIndex + 1) % backgrounds.length;
+        $('body').css('background-image', 'url("' + backgrounds[currentBgIndex] + '")');
+    });
+});
