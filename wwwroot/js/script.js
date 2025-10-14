62f59fd6359df9dafd1b2f5738c16cba418f4473
Lily-oswald <lilyoswald2005@gmail.com>
2025-10-14 08:39:56 -0500

    index.html

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -12,11 +12,27 @@ function displayCurrentTime() {
         width: 350
     });
 }
+function displayCurrentDate() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0'); 
+    const day = String(now.getDate()).padStart(2, '0');
+    const dateString = month + '/' + day + '/' + year;
+    
+    $('#time').html(dateString).css('visibility', 'visible');
+    
+    $('#time').dialog({
+        title: 'Current Date',
+        modal: true,
+        width: 350
+    });
+}
 
 $(document).ready(function() {
     // Time button click
     $('#timeBtn').click(displayCurrentTime);
-    
+    // Date button click
+    $('#dateBtn').click(displayCurrentDate);
     // Background images array
     const backgrounds = [
         'https://images.unsplash.com/photo-1516741539888-96b4c96adf0c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
