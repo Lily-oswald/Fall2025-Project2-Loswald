0842d53a45628c44489c193184573d995126c1a4
Lily-oswald <lilyoswald2005@gmail.com>
2025-10-14 10:37:11 -0500

    Program.cs

--- Program.cs
+++ Program.cs
@@ -14,10 +14,9 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseRouting();
-app.UseHttpsRedirection();
 app.UseDefaultFiles();
 app.UseStaticFiles();
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapStaticAssets();
